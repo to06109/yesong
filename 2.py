3d087e993fced85c88371c273ff7ac64fe634a28
to06109 <69625013+to06109@users.noreply.github.com>
2020-09-18 10:45:52 +0900

    test

---
+++ 2.py
@@ -0,0 +1,8 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 10:41:24) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> a=[1, 2, 3, 4, 5]
+>>> a
+[1, 2, 3, 4, 5]
+>>> 
